e769f30b39c6233b3b14572e3f15467986d18c23
jsuarez <jhovanny.suarez@keyrus.com>
2021-05-09 19:05:27 -0500

    Adding heroku

--- src/main/java/com/mercadolibre/reto/xmen/RetoXmenApplication.java
+++ src/main/java/com/mercadolibre/reto/xmen/RetoXmenApplication.java
@@ -10,13 +10,4 @@ public class RetoXmenApplication {
 		SpringApplication.run(RetoXmenApplication.class, args);
 	}
 
-//	@Override
-//	public void run(String... args) throws Exception {
-//		//String[] dna = {"ATGCGA","CAGTGC","TTATGT","AGAAGG","CCCCTA","TCACTG"};
-//		//String[] dna = {"ATGCGA","CAGTGC","TTATTT","AGACGG","GCGTCA","TCACTG"};
-//		//String[] dna = {"ATGCGA","AAGTGC","ATATTG","AGACGG","GCGTCG","TCACTG"};
-//		String[] dna = {"XAAAUA","AAGTGC","ATATTG","AGACGG","GCGTCG","TCACTT"};
-//		DNAEvaluatorServiceImpl dnaEvaluatorService = new DNAEvaluatorServiceImpl();
-//		System.out.println("Is mutant  :::::   " + dnaEvaluatorService.isMutant(dna));
-//	}
 }
